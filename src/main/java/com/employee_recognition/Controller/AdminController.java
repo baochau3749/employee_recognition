a22584f0c8909ad46d0abfe5f013ad7fa9459e6f
Carlos Leonard <CarlosLeonard33@outlook.com>
2018-09-27 21:47:46 -0400

    first commit

--- src/main/java/com/employee_recognition/Controller/AdminController.java
+++ src/main/java/com/employee_recognition/Controller/AdminController.java
@@ -6,6 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class AdminController {
 	
+	// new commit here
 	@GetMapping("/admin")
 	public String adminMainPage() {
 		return "admin";
