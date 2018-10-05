1057ae6612731a6073fbf44bd547700d66572243
Carlos Leonard <CarlosLeonard33@outlook.com>
2018-10-04 22:29:18 -0400

    test branch

--- src/main/java/com/employee_recognition/Controller/LoginController.java
+++ src/main/java/com/employee_recognition/Controller/LoginController.java
@@ -1,13 +1,18 @@
 package com.employee_recognition.Controller;
 
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 public class LoginController {
 	
 	@RequestMapping("/")
 	public String loginPage() {
+		
+		System.out.println("Hello World");
+		
 		return "login";
 	}
 	
